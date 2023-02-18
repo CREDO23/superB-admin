a62977c4c1e8767f7b4de6bf6547d2d059bdf2ae
CREDO23 <thierrybakera12@gmail.com>
2023-02-18 10:00:09 +0200

    setup project

---
+++ src/App.tsx
@@ -0,0 +1,12 @@
+
+
+function App() {
+
+  return (
+    <div className=" flex items-center overflow-auto no-scroll justify-center h-screen w-screen">
+      <h3 className=' text-fuchsia-800 text-3xl font-bold'>SUPER BEAUTE</h3>
+    </div>
+  );
+}
+
+export default App;
