113e3149a531ee267ddf053e7e674b6ca5080619
CREDO23 <thierrybakera12@gmail.com>
2023-02-19 20:19:34 +0200

    setup : antd color

--- src/main.tsx
+++ src/main.tsx
@@ -1,3 +1,4 @@
+import { ConfigProvider } from 'antd';
 import React from 'react';
 import ReactDOM from 'react-dom/client';
 import App from './App';
@@ -5,6 +6,14 @@ import './styles/index.scss';
 
 ReactDOM.createRoot(document.getElementById('root') as HTMLElement).render(
   <React.StrictMode>
+    <ConfigProvider theme={
+      {
+        token : {
+          colorPrimary : '#AC1E59'
+        }
+      }
+    }>
     <App />
+    </ConfigProvider>
   </React.StrictMode>
 );
