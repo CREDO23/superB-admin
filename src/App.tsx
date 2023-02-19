5a7422d1669952bf388123e10fd33e144272671e
CREDO23 <thierrybakera12@gmail.com>
2023-02-19 20:05:56 +0200

    setup : tailwind-theme color

--- src/App.tsx
+++ src/App.tsx
@@ -1,12 +1,9 @@
-
-
 function App() {
-
   return (
     <div className=" flex flex-col items-center overflow-auto no-scroll justify-center h-screen w-screen">
-      <h3 className=' text-rose text-3xl font-bold'>SUPER BEAUTE</h3>
-      <h3 className=' text-rose-5 text-3xl font-bold'>SUPER BEAUTE</h3>
-      <h3 className=' text-black text-3xl font-bold'>SUPER BEAUTE</h3>
+      <h3 className=" text-rose text-3xl font-bold">SUPER BEAUTE</h3>
+      <h3 className=" text-rose-5 text-3xl font-bold">SUPER BEAUTE</h3>
+      <h3 className=" text-black text-3xl font-bold">SUPER BEAUTE</h3>
     </div>
   );
 }
